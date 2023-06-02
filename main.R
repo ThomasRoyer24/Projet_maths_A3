13e2986dc93f1d2a01e2b4d03ddc83cc7f6d6089
foudroyeur <62610820+foudroyeur24@users.noreply.github.com>
2023-06-02 14:11:40 +0200

    end R

--- main.R
+++ main.R
@@ -1,20 +1,35 @@
-library(leaflet)
+library(shiny)
+library(threejs)
+tab_color <- c("#8EFFC3","#78FF60","#A9FF47","#F4FF28","#FF900A","#F44900","#EA1B0","#000000")
+app <- shinyApp(
+    ui = bootstrapPage(
+        globeOutput('plot')
+    ),
+    server = function(input, output) {
+        # Lecture du fichier CSV
+        data <- read.csv("seismes_2014.csv")
 
-# Créer une carte de base
-map <- leaflet()
+        latitude <- data[data$mag > 2, 2]
+        longitude <- data[data$mag > 2, 3]
+        mag <- as.integer(na.omit(data[data$mag > 2, 5]))
+        profondeur <- data[ , 6]
+        index_non_na <- !is.na(data[data$mag > 2, 5])
+        latitude_sans_na <- latitude[index_non_na]
+        longitude_sans_na <- longitude[index_non_na]
 
-# Ajouter des tuiles de fond (fond de carte)
-map <- addTiles(map)
 
-# Exemple de données de points avec différentes couleurs
-points <- data.frame(
-  lon = c(-73.9683, -74.0059, -71.0589),
-  lat = c(40.7851, 40.7128, 42.3601),
-  couleur = c("red", "green", "blue")
+        output$plot <- renderGlobe({
+            globejs(
+                lat = c(NA, latitude_sans_na),
+                long = c(NA, longitude_sans_na),
+                pointsize = 0.5,
+                color = tab_color[mag],
+                atmosphere = FALSE,
+                bg = "black",
+                value = profondeur / 4
+            )
+        })
+    }
 )
 
-# Ajouter les points à la carte avec des couleurs différentes
-map <- addCircleMarkers(map, data = points, lng = ~lon, lat = ~lat, color = ~couleur)
-
-# Afficher la carte
-map
+runApp(app, launch.browser = TRUE)
